b788fd0e046f473fdfde65ee49480f9ee797c584
john-langlois <joao.langlois@hotmail.com>
2022-11-27 15:17:12 -0500

    fixing component imports

--- server/routes/login.js
+++ server/routes/login.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
         }
         const user = await User.findOne({email:req.body.email})
         if(!user){
-            return res.status(401).send({message:"invalid Email or Password"
+            return res.status(401).send({message:"Invalid Email or Password"
         })}
         const validPassword = await bcrypt.compare(
             req.body.password,user.password);
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,19 +1,17 @@
 const router = require('express').Router();
-const{User,validate} = require('../models/user_model.js')
+const{User, validate} = require('../models/user_model.js')
 const bcrypt = require('bcrypt')
 
 router.post("/", async (req, res)=>{
     try{
-    //    const {error} = validate(req.body);
-    //    if(error){
-    //     return res.status(400).sendFile({message:error})
-    //    }
+       const {error} = validate(req.body);
+       if(error){
+        return res.status(400).sendFile({message:error})
+       }
        const user = await User.findOne({email:req.body.email})
        if(user){
         return res.status(409).send({message:"User with email already exists"})
        }
-       
-
        const salt = await bcrypt.genSalt(10);
        const hashPassword = await bcrypt.hash(req.body.password, salt)
         await new User({
